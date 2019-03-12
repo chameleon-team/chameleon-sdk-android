bbea71a490da9b24f3e92c6affb4cd8bad37420e
lizechen <lizechen@didichuxing.com>
2019-03-12 18:12:56 +0800

    减少rich模块大小，删除无用字体

--- rich-text-component/src/main/java/com/didi/chameleon/weex/richtextcomponent/utils/CmlFontUtil.java
+++ rich-text-component/src/main/java/com/didi/chameleon/weex/richtextcomponent/utils/CmlFontUtil.java
@@ -5,11 +5,10 @@ import android.graphics.Typeface;
 import android.text.TextUtils;
 
 /**
- * limeihong
  * create at 2018/10/11
  */
 public class CmlFontUtil {
-    private static final String TYPEFACE_BARLOW = "Barlow-Medium";
+    private static String typeName;
     private static Typeface typeface;
 
     /**
@@ -22,40 +21,22 @@ public class CmlFontUtil {
             return null;
         }
 
-        String fontNameAndroid = null;
-
-        // "dina"为富文本BtsRichInfo中font_name字段下发的，代表自定义字体Barlow-Medium，
-        // 因为android与ios两端用的字体不同，此字段内容只是一个代号，需要客户端解析
-        if ("dina".equals(fontName) || TYPEFACE_BARLOW.equals(fontName)) {
-            fontNameAndroid = TYPEFACE_BARLOW;
-        } else {
-            // 未来可能添加其他字体
-        }
-
-        if (fontNameAndroid == null) {
+        String fontPath = fontPath(fontName);
+        if (TextUtils.isEmpty(fontPath)) {
             return null;
         }
 
-        String fontPath = t("fonts/", fontNameAndroid, ".ttf");
-
-        if (typeface == null) {
+        if (!fontName.equals(typeName) || typeface == null) {
+            typeName = fontName;
             typeface = Typeface.createFromAsset(context.getAssets(), fontPath);
         }
 
         return typeface;
     }
 
-    /**
-     * 输出参数os拼接成的String
-     *
-     * @param os 参数数组
-     * @return 拼接结果
-     */
-    public static String t(Object... os) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (Object o : os) {
-            stringBuilder.append(o);
-        }
-        return stringBuilder.toString();
+    private static String fontPath(String fontName) {
+        //后续扩展，外部添加字体
+        return null;
     }
+
 }
